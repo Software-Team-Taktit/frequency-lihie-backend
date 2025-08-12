fea0fa3d3babdf741488d63e7af86a05ae0b33fd
lilushhh <lilyelimelech@gmail.com>
2025-08-12 13:48:00 +0300

    feat: added mission model

---
+++ models/domain/types/mission.py
@@ -0,0 +1,14 @@
+from uuid import uuid4, UUID
+from models.domain.base_object import BaseObject
+from models.enums.object_type import ObjectType
+from models.helpers.coordinate import Coordinate
+
+class Mission(BaseObject):
+    coordinate: Coordinate
+    enviroment_type: str
+    platform_id: UUID
+    
+    def __init__(self, **data):
+        data["id"] = data.get("id", uuid4())
+        data["type"] = ObjectType.MISSION
+        super().__init__(**data)

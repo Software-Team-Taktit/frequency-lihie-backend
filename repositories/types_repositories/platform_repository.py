7ac45796424f9006facb1292467cf2638d5c0a39
lilushhh <lilyelimelech@gmail.com>
2025-08-13 19:22:58 +0300

    feat: dry repo platform

---
+++ repositories/types_repositories/platform_repository.py
@@ -0,0 +1,7 @@
+from db.mongo import db
+from models.domain.types.platform import Platform
+from repositories.mongo_repository import MongoRepository
+
+class PlatformRepository(MongoRepository[Platform]):
+    def __init__(self):
+        super().__init__(collection=db["platforms"], model_cls=Platform)

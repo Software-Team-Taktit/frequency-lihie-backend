c8559d90e904467e724d7bd2949d2ef9cfcb63be
lilushhh <lilyelimelech@gmail.com>
2025-08-13 12:37:14 +0300

    feat: update user request

--- models/requests/user_request.py
+++ models/requests/user_request.py
@@ -1,8 +1,13 @@
-from uuid import uuid4, UUID
 from pydantic import BaseModel
 
 class UserCreateRequest(BaseModel):
-    personal_id: UUID
+    personal_id: str
+    first_name: str
+    last_name: str
+    unit: str
+    
+class UpdateUserRequest(BaseModel):
+    personal_id: str
     first_name: str
     last_name: str
     unit: str

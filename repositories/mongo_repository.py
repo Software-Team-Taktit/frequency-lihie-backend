c4e8e1ecab9ce7954573d8d628f82b6d76534000
lilushhh <lilyelimelech@gmail.com>
2025-08-13 19:10:17 +0300

    feat: mongo repo for all types repo

---
+++ repositories/mongo_repository.py
@@ -0,0 +1,31 @@
+from typing import TypeVar, Generic, List, Type
+from uuid import UUID
+from motor.motor_asyncio import AsyncIOMotorCollection
+from repositories.base_repository import BaseRepository
+
+T = TypeVar("T")
+
+class MongoRepository(BaseRepository[T], Generic[T]):
+    def __init__(self, collection: AsyncIOMotorCollection, model_cls: Type[T]):
+        self.collection = collection
+        self.model_cls = model_cls
+        
+    async def get_all(self):
+        cursor = self.collection.find({})
+        docs = await cursor.to_list(length=None)
+        return [self.model_cls(**doc) for doc in docs]
+    
+
+    async def add_item(self, item: T) -> T:
+        await self.collection.insert_one(item.model_dump())
+        return item
+    
+    async def delete_item(self, item_id: UUID) -> bool:
+        result = await self.collection.delete_one({"id": str(item_id)})
+        return result.deleted_count > 0
+
+    async def update_item(self, item_id: UUID, item: T) -> T:
+        result = await self.collection.replace_one({"id": str(item_id)}, item.model_dump())
+        if result.matched_count == 0:
+            raise ValueError(f"Item with id {item_id} not found")
+        return item

64a9cfe6aca9de96862f910f84531c898e0dfc4d
lilushhh <lilyelimelech@gmail.com>
2025-08-14 09:53:35 +0300

    bugfix: changed the id type to str from uuid

--- models/domain/base_object.py
+++ models/domain/base_object.py
@@ -1,8 +1,7 @@
 from pydantic import BaseModel, Field
 from abc import ABC
-from uuid import uuid4, UUID
 from models.enums.object_type import ObjectType
 
 class BaseObject(ABC, BaseModel):
-    id: UUID
+    id: str
     type: ObjectType 

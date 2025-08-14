35a35145559d02daaaf65f6ecabdf146b88573bc
lilushhh <lilyelimelech@gmail.com>
2025-08-14 12:47:24 +0300

    feat: finished base crud router

--- routers/base_crud_router.py
+++ routers/base_crud_router.py
@@ -21,11 +21,37 @@ class BaseCrudRouter(Generic[T, C, U]):
         self.router = APIRouter(prefix=f"/{entity_name}",tags=[entity_name])
         
         @self.router.post("")
-        async def create_item(payload: create_dto, repo: BaseRepository[T] = Depends(get_repository)):
+        async def create(payload: create_dto, repo: BaseRepository[T] = Depends(get_repository)):
             obj = self.model_cls(**payload.model_dump())
             saved = await repo.add_item(obj)
             if not saved:
-                raise HTTPException(status_code=400, detail=f"couldnt create {Type[T]}")
-            return {"message": f"item {saved.id} created"}
+                raise HTTPException(status_code=400, detail=f"couldnt create {self.model_cls.__name__}")
+            return saved.model_dump()
+        
+        @self.router.get("")
+        async def read_all(repo: BaseRepository[T] = Depends(get_repository)):
+            items = await repo.get_all()
+            return [item.model_dump() for item in items]
+        
+        @self.router.get("/{item_id}")
+        async def read_by_id(item_id:str, repo: BaseRepository[T] = Depends(get_repository)):
+            obj = await repo.get_by_id(item_id)
+            if not obj:
+                raise HTTPException(status_code=404, detail=f"{self.model_cls.__name__} not found")
+            return obj.model_dump()
+        
+        @self.router.put("/{item_id}")
+        async def update(item_id: str, payload: update_dto, repo: BaseRepository[T] = Depends(get_repository)):
+            obj = self.model_cls(id = item_id, **payload.model_dump())
+            updated = await repo.update_item(item_id, obj)
+            if not updated:
+                raise HTTPException(status_code=404, detail=f"faild to update {self.model_cls.__name__}")
+            return updated.model_dump()
+        
+        @self.router.delete("/{item_id}", status_code=204)
+        async def delete(item_id: str, repo: BaseRepository[T] = Depends(get_repository)):
+            deleted = await repo.delete_item(item_id)
+            if not deleted:
+                raise HTTPException(status_code= 404, detail=f"{self.model_cls.__name__} not found")
         
         

b3b09e6a64e847da42f22bd40afc5f5f23c2eb81
lilushhh <lilyelimelech@gmail.com>
2025-08-14 11:12:08 +0300

    initialize to base crud router

---
+++ routers/base_crud_router.py
@@ -0,0 +1,21 @@
+from typing import Generic, TypeVar, Type, Callable
+from fastapi import APIRouter, HTTPException, Depends
+from pydantic import BaseModel
+from repositories.base_repository import BaseRepository
+
+T = TypeVar("T") #Type
+C = TypeVar("C", bound=BaseModel) #Create
+U = TypeVar("U", bound=BaseModel) #Update
+
+class BaseCrudRouter(Generic[T, C, U]):
+    def __init__(self, 
+                entity_name: str,
+                model_cls: Type[T],
+                create_dto: Type[C],
+                update_dto: Type[U],
+                get_repository: Callable[[], BaseRepository[T]]
+            ):
+        self.model_cls = model_cls  
+        self.create_dto = create_dto
+        self.update_dto = update_dto
+        self.router = APIRouter(prefix=f"/{entity_name}",tags=[entity_name])

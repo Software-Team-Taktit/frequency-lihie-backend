560211f70cbe94a3885aed9ebbe8684dfb3ce19f
lilushhh <lilyelimelech@gmail.com>
2025-08-14 12:59:36 +0300

    feat: user router

---
+++ routers/types_routers/user_router.py
@@ -0,0 +1,16 @@
+from fastapi import Depends
+from ..base_crud_router import BaseCrudRouter
+from models.domain.types.users.user import User
+from models.requests.user_request import UserCreateRequest, UserUpdateRequest
+from repositories.types_repositories.user_repository import UserRepository
+
+def get_user_repo() -> UserRepository:
+    return UserRepository()
+
+user_router = BaseCrudRouter(
+    entity_name="users",
+    model_cls=User,
+    create_dto=UserCreateRequest,
+    update_dto=UserUpdateRequest,
+    get_repository=get_user_repo
+).router

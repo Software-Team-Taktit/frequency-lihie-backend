fa1b0f18fbba94f95c2b19112d36a04b3c9199c5
lilushhh <lilyelimelech@gmail.com>
2025-08-20 14:34:29 +0300

    feat: login router

--- routers/types_routers/user_router.py
+++ routers/types_routers/user_router.py
@@ -1,7 +1,8 @@
 from ..base_crud_router import BaseCrudRouter
 from models.domain.types.users.user import User
-from models.requests.user_request import UserCreateRequest, UserUpdateRequest
+from models.requests.user_request import UserCreateRequest, UserUpdateRequest, UserLogInRequest
 from repositories.types_repositories.user_repository import UserRepository
+from fastapi import Depends, HTTPException
 
 def get_user_repo() -> UserRepository:
     return UserRepository()
@@ -13,3 +14,10 @@ user_router = BaseCrudRouter(
     update_dto=UserUpdateRequest,
     get_repository=get_user_repo
 ).router
+
+@user_router.post("/login")
+async def login(dto: UserLogInRequest, repo: UserRepository = Depends(get_user_repo)):
+    user = await repo.get_by_personal_id(dto.personal_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="user not found")
+    return user.model_dump()

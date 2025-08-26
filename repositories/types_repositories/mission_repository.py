d0f022951dcdabbced51a9b990b33d3f312daed1
lilushhh <lilyelimelech@gmail.com>
2025-08-26 13:17:27 +0300

    feat: added validation that i cant delete platform that belongs to a mission

--- repositories/types_repositories/mission_repository.py
+++ repositories/types_repositories/mission_repository.py
@@ -5,3 +5,7 @@ from repositories.mongo_repository import MongoRepository
 class MissionRepository(MongoRepository[Mission]):
     def __init__(self):
         super().__init__(collection=get_db()["missions"], model_cls=Mission)
+        
+    async def exists_by_platform_id(self, platform_id: str):
+        return await self.collection.count_documents({"platform_id": platform_id}, limit = 1) > 0
+    
--- repositories/types_repositories/platform_repository.py
+++ repositories/types_repositories/platform_repository.py
@@ -1,7 +1,17 @@
 from db.mongo import get_db
 from models.domain.types.platform import Platform
+from fastapi import HTTPException
 from repositories.mongo_repository import MongoRepository
+from repositories.types_repositories.mission_repository import MissionRepository
+
 
 class PlatformRepository(MongoRepository[Platform]):
     def __init__(self):
         super().__init__(collection=get_db()["platforms"], model_cls=Platform)
+        self.mission_repo = MissionRepository()
+        
+        
+    async def delete_item(self, item_id: str):
+        if await self.mission_repo.exists_by_platform_id(item_id):
+            raise HTTPException(status_code=409, detail="cant delete platform that belongs to mission.")
+        return await super().delete_item(item_id)

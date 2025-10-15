e20979bebefbb526bfbe326e06bb540113fc3372
lilushhh <lilyelimelech@gmail.com>
2025-10-15 13:24:24 +0300

    feat: added name property to the mission requests

--- models/requests/mission_request.py
+++ models/requests/mission_request.py
@@ -1,13 +1,15 @@
 from uuid import UUID
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from models.helpers.coordinate import Coordinate
 
 class MissionCreateRequest(BaseModel):
+    name: str = Field(..., min_length=2, max_length=80)
     coordinate: Coordinate
     enviroment_type: str
     platform_id: str
     
 class MissionUpdateRequest(BaseModel):
+    name: str | None = Field(None, min_length=2, max_length=80)
     coordinate: Coordinate
     enviroment_type: str
     platform_id: str

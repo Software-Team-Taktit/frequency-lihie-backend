e3965c0985811e4174d5b386174b9d80f0aef5f7
lilushhh <lilyelimelech@gmail.com>
2025-10-15 13:23:04 +0300

    feat: added name property

--- models/domain/types/mission.py
+++ models/domain/types/mission.py
@@ -1,9 +1,11 @@
+from pydantic  import Field
 from uuid import uuid4, UUID
 from models.domain.base_object import BaseObject
 from models.enums.object_type import ObjectType
 from models.helpers.coordinate import Coordinate
 
 class Mission(BaseObject):
+    name: str = Field(..., min_length=2, max_length=80)
     coordinate: Coordinate
     enviroment_type: str
     platform_id: str

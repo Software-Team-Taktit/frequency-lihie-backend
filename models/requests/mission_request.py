7233c41f869aad672bc1de379aab3b07b0da0d5b
lilushhh <lilyelimelech@gmail.com>
2025-11-13 10:19:01 +0200

    feat: changed env type to the env type enum

--- models/requests/mission_request.py
+++ models/requests/mission_request.py
@@ -1,15 +1,16 @@
 from uuid import UUID
 from pydantic import BaseModel, Field
 from models.helpers.coordinate import Coordinate
+from models.enums.enviroment_type import EnviromentType
 
 class MissionCreateRequest(BaseModel):
     name: str = Field(..., min_length=2, max_length=80)
     coordinate: Coordinate
-    enviroment_type: str
+    enviroment_type: EnviromentType
     platform_id: str
     
 class MissionUpdateRequest(BaseModel):
     name: str | None = Field(None, min_length=2, max_length=80)
     coordinate: Coordinate
-    enviroment_type: str
+    enviroment_type: EnviromentType
     platform_id: str

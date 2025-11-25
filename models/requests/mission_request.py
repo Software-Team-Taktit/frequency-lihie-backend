79c6db912dbfecebd99ed7617ba62afd65aa7d93
lilushhh <lilyelimelech@gmail.com>
2025-11-25 12:18:41 +0200

    feat: added freq request

--- models/requests/mission_request.py
+++ models/requests/mission_request.py
@@ -3,6 +3,16 @@ from pydantic import BaseModel, Field
 from models.helpers.coordinate import Coordinate
 from models.enums.enviroment_type import EnviromentType
 
+class FrequencyRequest(BaseModel):
+    """
+    Request sent from the Hamal client in order to calculate
+    an optimal frequency and transmit power for a new mission.
+    """
+    name: str = Field(..., min_length=2, max_length=80)
+    coordinate: Coordinate
+    enviroment_type: EnviromentType
+    platform_id: str
+
 class MissionCreateRequest(BaseModel):
     name: str = Field(..., min_length=2, max_length=80)
     coordinate: Coordinate

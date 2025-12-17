0958673267137097fa276e55830006a84663256a
lilushhh <lilyelimelech@gmail.com>
2025-12-17 17:31:03 +0200

    feat: added admin login request

--- models/requests/admin_request.py
+++ models/requests/admin_request.py
@@ -1,7 +1,10 @@
-from models.requests.user_request import UserCreateRequest, UserUpdateRequest
+from models.requests.user_request import UserCreateRequest, UserUpdateRequest, UserLogInRequest
 
 class AdminCreateRequest(UserCreateRequest):
     pass
     
 class AdminUpdateRequest(UserUpdateRequest):
+    pass
+
+class AdminLogInRequest(UserLogInRequest):
     pass

8b87e57d686bdb32fb04a9222696999a32ec6cd2
lilushhh <lilyelimelech@gmail.com>
2026-01-08 11:22:49 +0200

    feat: propagtion models params as constants params

---
+++ services/co_existing_service/utils/constants.py
@@ -0,0 +1,3 @@
+EGLI_PARAMS = ["distance_km", "freq_mhz", "tx_height_m", "rx_height_m"]
+HATA_PARAMS = ["distance_km", "freq_mhz", "tx_height_m", "rx_height_m"]
+FSPL_PARAMS = ["distance_km", "freq_mhz"]

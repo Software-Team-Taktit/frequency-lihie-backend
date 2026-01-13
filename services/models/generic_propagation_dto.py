9c6debf29541a89c7d718ff37f1a86df20dd1414
lilushhh <lilyelimelech@gmail.com>
2026-01-13 16:22:49 +0200

    feat: added model type

--- services/models/generic_propagation_dto.py
+++ services/models/generic_propagation_dto.py
@@ -1,7 +1,9 @@
 from abc import ABC
 from typing import Optional
+from .enums.pathloss_models_enum import PropagationModelType
 
 class GenericPropagationDTO(ABC):
+    model_type: PropagationModelType
     freq_mhz: float
     distance_km: float
     tx_height_m: Optional[float] = None

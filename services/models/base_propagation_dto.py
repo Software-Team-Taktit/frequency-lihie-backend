5e429d2bfc2b43e3ae4a6cd1b747e1eb9ebc465e
lilushhh <lilyelimelech@gmail.com>
2026-02-12 11:18:35 +0200

    fix: changed the bade propagation dto types

--- services/models/base_propagation_dto.py
+++ services/models/base_propagation_dto.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
+from pydantic import PositiveFloat
 
 class BasePropagationDTO(BaseModel):
-    freq_mhz: float
-    distance_km: float
+    freq_mhz: PositiveFloat
+    distance_km: PositiveFloat

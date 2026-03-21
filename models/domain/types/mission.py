69fd1a7364873d32ddca5818ebd41bf8e4e1f5ed
lilushhh <lilyelimelech@gmail.com>
2026-03-21 18:14:30 +0200

    refactor: added created at and expires at props

--- models/domain/types/mission.py
+++ models/domain/types/mission.py
@@ -1,8 +1,12 @@
+from datetime import datetime, timedelta, timezone
 from pydantic  import Field
 from uuid import uuid4, UUID
 from models.domain.base_object import BaseObject
 from models.enums.object_type import ObjectType
 from models.helpers.coordinate import Coordinate
+
+def utc_now() -> datetime:
+    return datetime.now(timezone.utc)
 
 class Mission(BaseObject):
     name: str = Field(..., min_length=2, max_length=80)
@@ -11,7 +15,18 @@ class Mission(BaseObject):
     tx_power_dbm: float
     platform_id: str
     
+    is_active: bool = True
+    created_at: datetime = Field(default_factory=utc_now)
+    expires_at: datetime | None = None
+    
     def __init__(self, **data):
         data["id"] = data.get("id", str(uuid4()))
         data["type"] = ObjectType.MISSION
+        
+        if "created_at" not in data:
+            data["created_at"] = utc_now()
+            
+        if data.get("time") and data.get("expires_at") is None:
+            data["expires_at"] = data["created_at"] + timedelta(minutes=data["time"])
+        
         super().__init__(**data)
